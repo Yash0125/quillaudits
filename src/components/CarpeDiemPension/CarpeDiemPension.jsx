a6f50eb225cc2978063437558f7e59278dc7b3cb
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2024-03-15 20:34:37 +0530

    style : To make responsive

--- src/components/CarpeDiemPension/CarpeDiemPension.jsx
+++ src/components/CarpeDiemPension/CarpeDiemPension.jsx
@@ -5,10 +5,16 @@ import Sidebar from '../Sidebar/Sidebar';
 
 const CarpeDiemPension = () => {
   return (
+    <>
     <div className='carpediempension-container'>
       <CarpeDiemPensionLeft/>
       <Sidebar/>
     </div>
+    <div className='carpediempension-mobile-container'>
+      <Sidebar/>
+      <CarpeDiemPensionLeft/>
+    </div>
+    </>
   )
 }
 
